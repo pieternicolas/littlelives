78bcce00e479063d479d9fb1c73c1666336fdcb6
Nicolas Pieter <nicolaspieter@yahoo.com>
2024-10-16 18:56:49 +0700

    Start settings form

---
+++ src/modules/SettingsForm/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-full flex p-12">afsdfds</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps();
+
+const count = ref(0);
+</script>
+
+<style scoped></style>
